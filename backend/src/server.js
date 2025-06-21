b729123372de52c3323353e34c92b48a39c144d4
Anath <kyenshakhashimu@gmail.com>
2025-06-21 08:07:04 +0100

    still debugging

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,7 +38,7 @@ if(process.env.NODE_ENV === 'production'){
 
     app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-})
+});
 
 }
 connectDB().then(() => {
